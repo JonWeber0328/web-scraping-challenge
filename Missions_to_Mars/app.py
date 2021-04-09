eac0aff2cf03f86a6ce088c823630c5af519757e
Jon Weber <jonweber0328@gmail.com>
2021-04-09 12:08:04 -0500

    MongoDB connection working.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,7 +5,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/marsData"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -19,7 +19,7 @@ def scraper():
     
     listings = mongo.db.listings
     listings_data = scrape_mars.scrape()
-    print(listing_data)
+    print(listings_data)
     listings.update({}, listings_data, upsert=True)
     return redirect("/", code=302)
 
